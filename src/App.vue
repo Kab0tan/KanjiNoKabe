3770325af1e1d12b59e6341935709a6f930f3b2c
Justin <justinleddet@gmail.com>
2025-01-13 09:50:57 +0100

    Delete extra files + css fix

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <ElMenu mode="horizontal">
-    <ElMenuItem index="1">
-      <RouterLink to="/home"> Home </RouterLink>
-    </ElMenuItem>
-    <ElMenuItem index="2" route="/kanji">
+    <ElMenuItem index="1" route="/kanji">
       <RouterLink to="/kanji"> Kanji Quiz </RouterLink>
     </ElMenuItem>
   </ElMenu>
--- src/views/KanjiView/KanjiLeftPanel/KanjiLeftPanel.vue
+++ src/views/KanjiView/KanjiLeftPanel/KanjiLeftPanel.vue
@@ -1,11 +1,13 @@
 <template>
-  <ElRadioGroup v-model="level" class="level">
-    <ElRadio :value="1" size="large"> N1 </ElRadio>
-    <ElRadio :value="2" size="large"> N2 </ElRadio>
-    <ElRadio :value="3" size="large"> N3 </ElRadio>
-    <ElRadio :value="4" size="large"> N4 </ElRadio>
-    <ElRadio :value="5" size="large"> N5 </ElRadio>
-  </ElRadioGroup>
+  <div class="left-kanji__container">
+    <ElRadioGroup v-model="level" class="level">
+      <ElRadio :value="1" size="large"> N1 </ElRadio>
+      <ElRadio :value="2" size="large"> N2 </ElRadio>
+      <ElRadio :value="3" size="large"> N3 </ElRadio>
+      <ElRadio :value="4" size="large"> N4 </ElRadio>
+      <ElRadio :value="5" size="large"> N5 </ElRadio>
+    </ElRadioGroup>
+  </div>
 </template>
 
 <script setup>
